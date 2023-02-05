36bcd53207ecb35b5865f985dcd66fef9ca3b5f2
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-02-06 02:13:48 +0530

    Added solution - LeetHub

---
+++ Largest odd number in string - GFG/largest-odd-number-in-string.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String s = br.readLine();
+            Solution obj = new Solution();
+            System.out.println(obj.maxOdd(s));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    String maxOdd(String num) {
+        // code here
+         for(int i=num.length()-1;i>=0;i--){
+            if(num.charAt(i)%2!=0){
+                return num.substring(0,i+1);
+            }
+        }
+        return "";
+    }
+}
