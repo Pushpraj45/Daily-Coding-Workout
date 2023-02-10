d3f193e894dfdf4e345ebb80782339aa066046ee
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-02-10 23:49:35 +0530

    Added solution - LeetHub

---
+++ Circular tour - GFG/circular-tour.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+class First_Circular_tour
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t =sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			sc.nextLine();
+			String str = sc.nextLine();
+			String arr[] = str.split(" ");
+			int p[] = new int[n];
+			int d[] = new int[n];
+			int j=0;
+			int k=0;
+			for(int i=0; i<2*n; i++)
+			{
+				if(i%2 == 0)
+				{
+					p[j]= Integer.parseInt(arr[i]);
+					j++;
+				}
+				else
+				{
+					d[k] = Integer.parseInt(arr[i]);
+					k++;
+				}
+			}
+			
+			System.out.println(new Solution().tour(p,d));
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+// In java function tour() takes two arguments array of petrol
+// and array of distance
+
+class Solution
+{
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(int petrol[], int distance[])
+    {
+	// Your code here
+	int n=petrol.length;
+	int a=0, b=0,c=0;
+	for(int i=0; i<n; i++){
+	    b+=petrol[i]-distance[i];
+	    if(b<0){
+	        a=i+1;
+	        c+=b;
+	        b=0;
+	        
+	    }
+	    
+	}
+	return ((b+c)>=0) ? (a):-1;
+    }
+}
