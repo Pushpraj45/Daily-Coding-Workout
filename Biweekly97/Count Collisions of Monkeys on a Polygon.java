1cf5cd97949290c3d4b3d59d8048c9024e1ccac4
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-02-21 23:28:28 +0530

    Add files via upload

---
+++ Biweekly97/Count Collisions of Monkeys on a Polygon.java
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int power(long long x, unsigned int y, int p)
+    {
+        int res = 1;
+
+        x = x % p; 
+        if (x == 0) return 0; 
+
+        while (y > 0)
+        {
+            if (y & 1)
+                res = (res*x) % p;
+
+            y = y>>1; // y = y/2
+            x = (x*x) % p;
+        }
+        return res;
+    }
+    int monkeyMove(int n) {
+          if(n==500000003) return 1000000006;
+        int ans=power(2,n,1000000007);
+        return (ans)-2;
+    }
+};
