ae360150f517b47e31e020bb178941ec0ac58bc4
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-06-20 10:30:40 +0530

    Time: 1 ms (100.00%) | Memory: 45 MB (46.42%) - LeetSync

---
+++ move-zeroes/move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int k=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=0){
+            int temp = nums[k];
+                nums[k] = nums[i];
+                nums[i] = temp;
+                k++;
+        }
+    }
+}
+}
