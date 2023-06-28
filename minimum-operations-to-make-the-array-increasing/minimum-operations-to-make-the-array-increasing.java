9d4c012c94922475ff709af115d6ad0bf7b29f96
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-06-29 00:36:09 +0530

    Time: 2 ms (93.28%) | Memory: 44.3 MB (25.92%) - LeetSync

--- minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
+++ minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -6,8 +6,11 @@ class Solution {
         int count = 0;
         for(int i=1; i<nums.length; i++){
            if(nums[i]<=nums[i-1]){
+            //    This is the minimum increment we have to do to make our array strictly decreasing 
                int c=nums[i-1]-nums[i]+1;
+            //    Added incremented value to the element of our array and updated it 
                nums[i]+=c;
+            //    Added it in our ans as well 
                count+=c;
            }
         }
