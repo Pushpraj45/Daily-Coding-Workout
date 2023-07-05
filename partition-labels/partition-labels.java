469b7f12939d9281e087aafbf3674ccf5ee96b39
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-07-05 23:19:27 +0530

    Time: 6 ms (59.00%) | Memory: 41.5 MB (45.41%) - LeetSync

--- partition-labels/partition-labels.java
+++ partition-labels/partition-labels.java
@@ -1,41 +1,23 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        // HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        // for(int i=0; i<s.length(); i++){
-        //     map.put(s.charAt(i), i);
-        // }
-
-        // List<Integer> ans = new ArrayList<>();
-        // int i=0,j=0;
-        // for(int k=0; k<s.length(); k++){
-        //     j=Math.max(j, map.get(s.charAt(k)));
-
-        //     if(k==j){
-        //         ans.add(j-i+1);
-        //         i+=k;
-        //     }
-        // }
-        // return ans;
-         Map<Character, Integer> lastIndexMap = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            lastIndexMap.put(s.charAt(i), i);
+        for(int i=0; i<s.length(); i++){
+            map.put(s.charAt(i), i);
         }
 
-        List<Integer> result = new ArrayList<>();
-        int start = 0;
-        int end = 0;
+        List<Integer> ans = new ArrayList<>();
+        int i=0,j=0;
+        for(int k=0; k<s.length(); k++){
+            j=Math.max(j, map.get(s.charAt(k)));
 
-        for (int i = 0; i < s.length(); i++) {
-            end = Math.max(end, lastIndexMap.get(s.charAt(i)));
-
-            if (i == end) {
-                result.add(end - start + 1);
-                start = i + 1;
+            if(k==j){
+                ans.add(j-i+1);
+                i=k+1;
             }
         }
-
-        return result;
+        return ans;
+        
         
 }
 }
