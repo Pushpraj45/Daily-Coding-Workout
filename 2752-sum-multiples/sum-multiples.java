ea734a66188e54c5d2f00982584d41ff218e33eb
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-07-23 00:36:14 +0530

    Time: 8 ms (5.12%) | Memory: 43.6 MB (5.05%) - LeetSync

---
+++ 2752-sum-multiples/sum-multiples.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        ArrayList<Integer> arr=new ArrayList<>();
+
+        for(int i=0; i<=n; i++){
+            if(i%3==0 || i%5==0 || i%7==0){
+                arr.add(i);
+            }
+        }
+        int ans=0;
+        for(int i=0; i<arr.size(); i++){
+            ans+=arr.get(i);
+        }
+        return ans;
+    }
+}
