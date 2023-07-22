7e1640e44f3c530e0955f43b8e900ef40d8d2fde
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-07-23 01:35:38 +0530

    Time: 1 ms (87.69%) | Memory: 39.6 MB (61.72%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int sum=0;
+        for(int i=1; i<=n; i++){
+            sum+=i;
+        }
+        int temp=0;
+        for(int i=1; i<=n; i++){
+            sum=sum-i;
+            if(temp!=sum){
+                temp+=i;
+            }
+            else{
+            return i;
+            }
+        }
+    return -1;
+    }
+}
