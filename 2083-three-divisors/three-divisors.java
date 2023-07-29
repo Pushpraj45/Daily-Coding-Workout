4e47a1ef54a2004f9f22f29300bdbeee403f0d7e
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-07-29 12:43:44 +0530

    Time: 1 ms (79.55%) | Memory: 39.1 MB (85.71%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isThree(int n) {
+        if(n==1 || n==2){
+            return false;
+        }
+        int count=0;
+        for(int i=2; i<=n/2; i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        if(count==1){
+            return true;
+        }
+        return false;
+    }
+}
+
+/*
+n is not a perfect square? Return False.
+n is a perfect square? Okay, what about sqrt(n)?
+sqrt(n) is a prime number - return True.
+sqrt(n) is not a prime number - return False.
+*/
