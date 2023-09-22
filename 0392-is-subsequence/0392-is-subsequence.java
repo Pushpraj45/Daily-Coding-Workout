c51329401754070e6f3cf399fc44fb43df762669
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-09-23 00:04:50 +0530

    Time: 1 ms (89.69%), Space: 40.4 MB (67.46%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0){
+         return true;
+        }
+
+        int i = 0, j = 0;
+        while (j < t.length()) {
+            if (t.charAt(j) == s.charAt(i)) {
+                i++;
+                if (i == s.length()) return true;
+            }
+            j++;
+        }
+        return false;
+    }
+}
