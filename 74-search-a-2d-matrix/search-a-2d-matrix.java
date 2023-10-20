604211c2b34e8ef32bed2fed2457b6870a6a6982
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-10-20 14:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (26.94%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i=0;
+        int j=n-1;
+
+        while(i>=0 && i<m && j>=0 && j<n){
+            if(matrix[i][j] == target){
+                return true;
+            }
+            else if(matrix[i][j]<target){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return false;
+    }
+}
