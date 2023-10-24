37b4de4834d55d46c3972a3cde614717598c83a9
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-10-24 14:33:38 +0530

    Added solution - LeetHub

--- Longest K unique characters substring - GFG/longest-k-unique-characters-substring.java
+++ Longest K unique characters substring - GFG/longest-k-unique-characters-substring.java
@@ -24,48 +24,41 @@ class GfG {
 class Solution {
     public int longestkSubstr(String s, int k) {
         // code here
-         HashMap<Character,Integer> hm=new HashMap<>();
-        int i =0;
-        int j =0;
-        int maxStringlen=-1;
-        while(j<s.length()){
-            char ch=s.charAt(j);
-            if(hm.containsKey(ch)){
-                int freq=hm.get(ch);
-                freq++;
-                hm.put(ch,freq);
-            }
-            else{
-                hm.put(ch,1);
-            }
+        int n = s.length();
+        int ans = -1, i=0, j=0;
+        
+        HashMap<Character, Integer> hm = new HashMap<>(); // Map to store the frequency character
+        
+        while(j<n){
+            // Calculation based on conditions given 
+            char ch  = s.charAt(j);
+            // Get the frequency of ch from Map and increase it 
+            hm.put(ch, hm.getOrDefault(ch,0)+1);
             
             if(hm.size()<k){
                 j++;
             }
+            
             else if(hm.size()==k){
-                maxStringlen=Math.max(maxStringlen,j-i+1);
-               j++;
+                ans = Math.max(ans, j-i+1);
+                j++;
             }
-            else if(hm.size()>k ){
-                while(hm.size()>k && i<s.length()){
-                
-                if(hm.containsKey(s.charAt(i))){
-                    int freq=hm.get(s.charAt(i));
-                    freq--;
-                    hm.put(s.charAt(i),freq);
+            else if(hm.size()>k){
+                while(hm.size()>k){
+                    if(hm.containsKey(s.charAt(i))){
+                        int f = hm.get(s.charAt(i));
+                        f--;
+                        hm.put(s.charAt(i), f);
+                    }
+                    if(hm.get(s.charAt(i))==0){
+                        hm.remove(s.charAt(i));
+                    }
+                    i++;
                 }
-                 if(hm.get(s.charAt(i))==0){
-                    hm.remove(s.charAt(i));
-                }
-                
-                i++;
-                
+                j++;
             }
-            j++;
-            }
-            
-            
         }
-        return maxStringlen;
+        return ans; 
+         
     }
 }
