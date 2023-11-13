ac39d7966f302d95dda48a562d368f6f85aeaa46
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-11-13 22:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 38.6 MB (97.81%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int distance = 0;
+        while(x>0 || y>0){
+            if((x&(1))!= (y&(1))){
+                distance++;
+            }
+
+            x=x>>1;
+            y=y>>1;
+        }
+
+        return distance; 
+    }
+}
