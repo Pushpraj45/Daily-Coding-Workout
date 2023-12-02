676ef9e16985b901f03ceecdbc21d881caccb688
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2023-12-02 23:57:37 +0530

    Time: 10 ms (52.96%) | Memory: 44.5 MB (21.73%) - LeetSync

---
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] char_map;
+    public int countCharacters(String[] words, String chars) {
+        char_map=new int[26];
+       int ans=0;
+       for(char c : chars.toCharArray()){
+           char_map[c-'a']++;
+       }
+
+       for(String word: words){
+           int[] dummyArray = Arrays.copyOf(char_map, char_map.length);
+           int count =0;
+           for(char c: word.toCharArray()){
+               if(dummyArray[c-'a']>0){
+                   count++;
+                   dummyArray[c-'a']--;
+               }
+           }
+           if(count==word.length()){
+               ans+=word.length();
+           }
+       }
+       return ans;
+    }
+}
