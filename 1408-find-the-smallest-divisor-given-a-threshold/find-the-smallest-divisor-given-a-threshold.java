47fffd81adbf4e43dc27ed83ff280c2c6829a0d0
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-02-10 11:57:38 +0530

    Time: 34 ms (12.10%) | Memory: 47.9 MB (73.14%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int smallestDivisor(int[] arr, int limit) {
+        int n = arr.length; //size of array.
+        if(n > limit) return -1;
+        //Find the maximum element:
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        int low = 1, high = maxi;
+
+        //Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sumByD(arr, mid) <= limit) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    public int sumByD(int[] arr, int div){
+        int n = arr.length;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += Math.ceil((double)(arr[i]) / (double)(div));
+        }
+        return sum;
+
+    }
+
+}
