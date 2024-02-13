47f431a785796255d7bbb53e70e90daa0664ef20
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-02-13 11:20:13 +0530

    Time: 2 ms (99.91%) | Memory: 42.2 MB (50.28%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,26 +1,49 @@
+// class Solution {
+//     public String removeOuterParentheses(String s) {
+//         String ans="";
+//         int mycount = 0;
+//         for(int i=0; i<s.length(); i++){
+//             if(s.charAt(i)=='(' && mycount==0){
+//                 mycount++;
+//             }
+//             else if(s.charAt(i)=='(' && mycount>=1) // Another opening parenthese is encountered so adding it in ans
+//             {
+//                 ans+=s.charAt(i); 
+//                 mycount++;
+//             }
+//             else if(s.charAt(i)==')' && mycount>1){
+//                 ans+=s.charAt(i); // As one closing parenthese is encountered adding it and mycount decrement
+//                 mycount--;
+//             }
+//             else if(s.charAt(i)==')' && mycount==1){
+//                 mycount--;
+//             }
+
+//         }
+//             return ans;
+
+//     }
+// }
+
 class Solution {
     public String removeOuterParentheses(String s) {
-        String ans="";
-        int mycount = 0;
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)=='(' && mycount==0){
-                mycount++;
-            }
-            else if(s.charAt(i)=='(' && mycount>=1) // Another opening parenthese is encountered so adding it in ans
-            {
-                ans+=s.charAt(i); 
-                mycount++;
-            }
-            else if(s.charAt(i)==')' && mycount>1){
-                ans+=s.charAt(i); // As one closing parenthese is encountered adding it and mycount decrement
-                mycount--;
-            }
-            else if(s.charAt(i)==')' && mycount==1){
-                mycount--;
-            }
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
 
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (cnt > 0) {
+                    sb.append(c);
+                }
+                cnt++;
+            } else if (c == ')') {
+                cnt--;
+                if (cnt > 0) {
+                    sb.append(c);
+                }
+            }
         }
-            return ans;
 
+        return sb.toString();
     }
 }
