ea2d1adc29ac86cd0e5ce4d2250d296898ec6bb9
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-03-08 23:29:31 +0530

    Time: 6 ms (95.02%) | Memory: 55.1 MB (43.88%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int totalFruit(int[] arr) {
+    public int totalFruit(int[] tree) {
+        /* Typical Sliding window as the size of bucket is 2 only 
         int n = arr.length;
         int ans = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -21,5 +22,36 @@ class Solution {
         }
 
         return ans; 
+        */
+
+        int lastfruit = -1;
+        int secondlastfruit = -1;
+        int lastfruitcnt = 0;
+        int currmax = 0;
+        int max = 0;
+
+        for(int fruit:tree){
+            if(fruit==lastfruit || fruit == secondlastfruit){
+                currmax+=1;
+            }
+            else {
+                currmax = lastfruitcnt+1;
+            }
+
+            if(fruit == lastfruit){
+                lastfruitcnt+=1;
+            }
+            else{
+                lastfruitcnt = 1;
+            }
+
+            if(fruit!=lastfruit){
+                secondlastfruit = lastfruit;
+                lastfruit = fruit;
+            }
+            max = Math.max(max,currmax);
+        }
+
+        return max;
     }
 }
