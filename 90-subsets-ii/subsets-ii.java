89283de23646f5995003253f31fde87f83e6a5eb
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-03-13 23:19:59 +0530

    Time: 1 ms (99.86%) | Memory: 43.4 MB (63.06%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,20 @@
+class Solution{
+    public void printsubsetsum2(int index,int nums[],List<List<Integer>> ans,ArrayList<Integer> ds){
+        ans.add(new ArrayList<>(ds));
+
+        //main
+        for(int i=index;i<nums.length;i++){
+            if(i!=index && nums[i]==nums[i-1]) continue;
+            ds.add(nums[i]);
+            printsubsetsum2(i+1,nums,ans,ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        printsubsetsum2(0,nums,ans,new ArrayList<>());
+        return ans;
+
+    }
+}
