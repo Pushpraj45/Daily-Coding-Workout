27e82036ab54d5beeef006e617a3ffb29a140bd5
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-03-13 14:06:31 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (13.37%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+         List<List<Integer>> ans=new ArrayList<>();
+        getsubsets(0, nums, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private void getsubsets(int i, int[] arr, List<Integer> ds, List<List<Integer>>ans){
+        if(i == arr.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        ds.add(arr[i]);
+        getsubsets(i+1,arr,ds,ans); // Pick condition
+        ds.remove(ds.size()-1);
+        getsubsets(i+1,arr,ds,ans); // Not pick
+    }
+}
