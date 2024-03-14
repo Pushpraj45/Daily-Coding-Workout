216b7c093e2443326aa7fb7f43c6227be56ec650
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-03-14 11:47:21 +0530

    Time: 1 ms (95.45%) | Memory: 44.4 MB (47.97%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,14 +1,15 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permute(int[] nums) { 
+        
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
-        boolean freq[] = new boolean[nums.length]; // to check which element has been taken and which is left
+        // List<Integer> ds = new ArrayList<>();
+        // boolean freq[] = new boolean[nums.length]; // to check which element has been taken and which is left
 
-        func(nums, ds, ans, freq);
+        func(0,nums,ans);
 
         return ans;
     }
-
+/*
     private void func(int[] arr, List<Integer> ds, List<List<Integer>> ans, boolean[] f){
         if(ds.size()==arr.length){
             ans.add(new ArrayList<>(ds));
@@ -21,8 +22,35 @@ class Solution {
                 ds.add(arr[i]);
                 func(arr,ds,ans,f); // again calling so that hamare ds mein sab store rahe
                 ds.remove(ds.size()-1); // repetative permutations nahi aane chahiye na 
-                f[i] = false;
+                f[i] = false; // Ye jab ek full function call ho chuki hogi tab backtrack karte time ke liye phir se i=2 ke liye hai and so on..
             }
         }
+        */ 
+    
+    private void func(int index, int[] arr, List<List<Integer>> ans){
+        if(index == arr.length){
+            // copy the ds to ans
+            List<Integer> ds = new ArrayList<>();
+            for(int i=0; i<arr.length; i++){
+                ds.add(arr[i]);
+            }
+
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for(int i=index; i<arr.length; i++){
+            swap(i,index,arr);
+            func(index+1,arr,ans); // Calling to print all permutations
+            swap(i,index,arr); // Reswapping while backtracking into the problem
+        }
     }
+
+    private void swap(int i, int j, int[] nums){
+        int t=nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+
 }
