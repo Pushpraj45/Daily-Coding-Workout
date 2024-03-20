fd30141fce0d32b6239117b839e5110d4ca7487a
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-03-20 22:58:48 +0530

    Time: 2 ms (40.65%) | Memory: 46.2 MB (94.30%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.java
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.java
@@ -9,28 +9,64 @@
  * }
  */
 class Solution {
-     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-        if(list1 == null || list2 == null){
-            return null;
-        }
-        ListNode slow,fast;
-        slow=list1;
-        fast=list1;
-        int k=b-a+1;
-        for(int i=0;i<k;i++)
-            fast=fast.next;
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode curr = list1;
+        ListNode NewNode1 = list1;
+        ListNode NewNode2 = list1;
         
-        for(int i=1;i<a;i++)
-		{ 
-            slow=slow.next;
-            fast=fast.next;
+        for(int i=0; i<b; i++){
+            if(i==a-1)  NewNode1 = curr;
+            curr = curr.next;
         }
-        slow.next=list2;
-        while(list2.next!=null)
-            list2=list2.next;
-			
-        list2.next=fast.next;
-        
+        NewNode2 = curr.next;
+        NewNode1.next = list2;
+        while(list2.next!=null){
+            list2 = list2.next;
+        } 
+        list2.next = NewNode2;
         return list1;
+   
+      
+//         Another solution
+
+        // int count = 0;
+        // ListNode curr = list1;
+        // ListNode newVal = null;
+        // while(curr!=null){
+        //     if(count==b) newVal = curr.next;
+        //     count++;
+        //     curr = curr.next;
+        // }
+        // curr = list1; count=0;
+        // while(curr!=null){
+        //     count++;
+        //     if(count==a) curr.next = list2;
+        //     curr = curr.next; 
+        // }
+        // curr = list1;
+        // while(curr.next!=null){
+        //     curr = curr.next;
+        // }
+        // curr.next = newVal;
+        // return list1;
     }
 }
+//     same approach but using for loop
+
+        // ListNode head = list1;
+        // for(int i = 1; i < a; i++) {
+        //     head = head.next;
+        // }
+        // ListNode temp = head;
+        // for(int i = 0; i < b-a+2; i++) {
+        //     temp = temp.next;
+        // }
+        // ListNode temp2 = list2;
+        // while(temp2.next != null) {
+        //     temp2 = temp2.next;
+        // }
+        // head.next = list2;
+        // temp2.next = temp;
+        // return list1;
+
+        
