c271437cd7d7a539b40c6cb86ccbf31f7c41d10d
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-04-24 19:01:25 +0530

    Time: 3 ms (82.49%) | Memory: 46.7 MB (74.60%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,27 +1,40 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int start = 0;
-        int end = 0;
-        int countzeroes = 0;
+        // int start = 0;
+        // int end = 0;
+        // int countzeroes = 0;
 
-        while(end<nums.length){
-            if(nums[end]==0){
-                countzeroes++;
+        // while(end<nums.length){
+        //     if(nums[end]==0){
+        //         countzeroes++;
+        //     }
+        //     end++;
+
+        //     // if zeroes are crossing k then remove the calc of all zeroes
+        //     if(countzeroes>k){
+        //         if(nums[start]==0){
+        //             countzeroes--;
+
+        //         }
+
+        //         start++;
+        //     }
+        // }
+
+        // return end-start; // or we can count max value above also 
+         int l=0,r=0,max=0,zero=0,n=nums.length;
+        while(r<n){
+            if(nums[r]==0) zero++;
+            if(zero>k){
+            if(nums[l]==0) zero--;
+                l++;
             }
-            end++;
-
-            // if zeroes are crossing k then remove the calc of all zeroes
-            if(countzeroes>k){
-                if(nums[start]==0){
-                    countzeroes--;
-
-                }
-
-                start++;
+            if(zero<=k){
+                max=Math.max(max,r-l+1);
             }
+            r++;
         }
-
-        return end-start; // or we can count max value above also 
+    return max;
 
     }
 }
