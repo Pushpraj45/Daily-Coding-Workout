3f1b6442284ff1ad603856772ff7b1606777c28c
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-05-20 13:11:09 +0530

    Time: 1 ms (81.45%) | Memory: 42 MB (21.96%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -2,22 +2,50 @@ class Solution {
     public int maxProfit(int[] prices) {
          int n = prices.length;
         // BC not necessary as DP 0 se hi init rehti hai hamesha 
-        int[][]dp = new int[n+2][2]; // 3-> K+1
-        // Traverse from the opposite direction of the Memoization 
-        for(int ind=n-1; ind>=0; ind--){
-            for(int buy = 0; buy<=1; buy++){
-                 // cap ko 1 se till <=k 
-                    if(buy==0){
-                        dp[ind][buy] = Math.max(0+dp[ind+1][0], -prices[ind]+dp[ind+1][1]);
+        int[][]dp = new int[n+2][2]; 
+         for(int[] row: dp){
+          Arrays.fill(row, -1);
+        }
+        return MaximumProfitMMIII(prices,n,0,0,dp);
 
-                    }
-                    if(buy==1){
-                        dp[ind][buy] = Math.max(0+dp[ind+1][1], prices[ind]+dp[ind+2][0]);
-                    }
+        // Traverse from the opposite direction of the Memoization 
+        // for(int ind=n-1; ind>=0; ind--){
+        //     for(int buy = 0; buy<=1; buy++){
+               
+        //             if(buy==0){
+        //                 dp[ind][buy] = Math.max(0+dp[ind+1][0], -prices[ind]+dp[ind+1][1]);
+
+        //             }
+        //             if(buy==1){
+        //                 dp[ind][buy] = Math.max(0+dp[ind+1][1], prices[ind]+dp[ind+2][0]); // ind+2 tak chalegi bas ab ye 
+        //             }
                 
-            }
+        //     }
+        // }
+
+        // return dp[0][0]; 
+
+    }
+    // Memo code 
+     public static int MaximumProfitMMIII(int[] arr, int n, int index, int buy, int[][]dp){
+        // 3d dp for ind,buy, cap
+        if(index>=n){
+            return 0;
         }
 
-        return dp[0][0]; 
+        if(dp[index][buy]!=-1){
+            return dp[index][buy];
+        }
+
+        int profit= Integer.MIN_VALUE;
+        if(buy==0){
+            profit = Math.max(0+MaximumProfitMMIII(arr, n, index+1, 0, dp), -arr[index]+MaximumProfitMMIII(arr, n, index+1, 1, dp));
+        }
+        if(buy==1){
+            profit = Math.max(0+MaximumProfitMMIII(arr, n, index+1, 1,  dp), arr[index]+MaximumProfitMMIII(arr, n, index+2, 0, dp));
+        }
+
+        return dp[index][buy] = profit;
+
     }
 }
