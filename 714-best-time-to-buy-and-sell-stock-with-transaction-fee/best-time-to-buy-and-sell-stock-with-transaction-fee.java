28fbf162e5759bc611433e83827fe0f72653575b
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-05-20 13:24:00 +0530

    Time: 19 ms (45.15%) | Memory: 55 MB (64.20%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,14 +1,36 @@
 class Solution {
-    public int maxProfit(int[] prices, int fee) {
-        int n = prices.length;
-        int dp[][] = new int[n][2];
+    public int maxProfit(int[] arr, int fee) {
+        int n = arr.length;
+        // int dp[][] = new int[n][2];
         
-        // Initialize dp array with -1 to mark states as not calculated yet
-        for (int row[] : dp) {
-            Arrays.fill(row, -1);
+        // // Initialize dp array with -1 to mark states as not calculated yet
+        // for (int row[] : dp) {
+        //     Arrays.fill(row, -1);
+        // }
+        //  int ans = MaximumProfitAfterFees(prices, 0, 0, n, fee, dp);
+        // return ans;
+         if(n==0)
+        {
+            return 0;
+
         }
-         int ans = MaximumProfitAfterFees(prices, 0, 0, n, fee, dp);
-        return ans;
+
+        int[][] dp = new int[n+1][2];
+
+        for(int index = n-1; index>=0; index--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy==0){
+                    profit = Math.max(0+dp[index+1][0], -arr[index]+dp[index+1][1]);
+                }
+                if(buy==1){
+                    profit = Math.max(0+dp[index+1][1], arr[index]-fee+dp[index+1][0]);
+                }
+
+                dp[index][buy]=profit;
+            }
+        }
+        return dp[0][0]; // That will be our answer brooo 
     }
       public int MaximumProfitAfterFees(int[] arr, int index, int buy, int n, int fees, int[][] dp){
         if(index==n){
