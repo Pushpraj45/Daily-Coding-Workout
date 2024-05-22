821f059c97d37be4f9baf1050eb9f5dea9b3bc0a
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-05-23 00:16:27 +0530

    Time: 9 ms (30.15%) | Memory: 47.5 MB (51.20%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,29 +1,30 @@
 class Solution {
     public int change(int sum, int[] coins) {
-         int n=coins.length;
-        int dp[][]=new int[n+1][sum+1];
+        int n = coins.length;
+        int[][] dp = new int[n+1][sum+1];
 
-//        initialize - sum is 0
-//        i-> coins j-> sum/change
-
-        for (int i=0; i<n+1; i++){
-            dp[i][0]=1;
-        }
-
-        for (int j=1; j<sum+1; j++){
-            dp[0][j]=0;
-        }
-
-        for (int i=1; i<n+1; i++){
-            for (int j=1; j<sum+1; j++){
-                if (coins[i-1]<=j){ //valid
-                    dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=sum; j++){
+                if(i==0){
+                    dp[i][j]=0;
                 }
-                else{
-                    dp[i][j]=dp[i-1][j];
+                if(j==0){
+                    dp[i][j] = 1; // sum is 0 there is always a way 
                 }
             }
         }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sum; j++){
+                if(coins[i-1]<=j){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
         return dp[n][sum];
     }
 }
