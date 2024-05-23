a524034d66283354abad59f3a7efa920eb0b2184
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-05-23 11:22:49 +0530

    Time: 16 ms (31.69%) | Memory: 48.9 MB (43.32%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -4,13 +4,39 @@ class Solution {
     public int numDistinct(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        int[][] dp = new int[n][m];  // Correct dimensions
+        // int[][] dp = new int[n][m];  // Correct dimensions
 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        // for (int[] row : dp) {
+        //     Arrays.fill(row, -1);
+        // }
+
+        // return NoOfDisSubMemo(s1, s2, n - 1, m - 1, dp);
+
+              int[][] dp = new int[n+1][m+1];
+
+        // Init the first column with 1 becuase there's one empty subsequence in any string
+        for(int i=0; i<n+1; i++){
+            dp[i][0] = 1;
+        }
+        // Init the first row except dp[0][0] with 0 because there is not way to form s2 from an empty string s1
+        for(int j=1; j<m+1; j++){
+            dp[0][j] = 0; 
         }
 
-        return NoOfDisSubMemo(s1, s2, n - 1, m - 1, dp);
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    // Either exclude or include for further check
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%prime;
+                }
+                else{
+                    // If the characters don't match, we can only exclude this character.
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 
     public static int NoOfDisSubMemo(String x, String y, int ind1, int ind2, int[][] dp) {
