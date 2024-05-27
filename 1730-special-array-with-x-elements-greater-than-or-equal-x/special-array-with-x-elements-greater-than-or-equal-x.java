475735bd6889510666cd7d39b8f2d02d77369420
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-05-27 18:40:40 +0530

    Time: 1 ms (78.32%) | Memory: 41.6 MB (12.47%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        int[] bucket = new int[1001];
+        for(int num : nums){
+            bucket[num]++;
+        }
+        int total = nums.length;
+        for(int i=0; i<1001; i++){
+            if(i==total){
+                return i;
+            }
+            total-=bucket[i];
+        }
+        return -1;
+    }
+}
