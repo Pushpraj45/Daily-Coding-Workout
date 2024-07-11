77ce473c624f20d3d0ad5357ef679cc3cf560241
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-07-11 07:51:47 +0530

    Time: 36 ms (87.93%) | Memory: 47.3 MB (56.50%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,30 +1,42 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        // Will be doing this using atmost K technique
-         return atmost(nums,k)-atmost(nums,k-1);
+        // int count = 0;
+        // for (int i = 0; i < nums.length; i += 1) {
+        //     Set<Integer> set = new HashSet<>();
+        //     for (int j = i; j < nums.length; j += 1) {
+        //         set.add(nums[j]);
+        //         if (set.size() == k) {
+        //             count += 1;
+        //         }
+        //     }
+        // }
+        // return count;
+         return atmost(nums, k) - atmost(nums, k - 1);
     }
 
     private int atmost(int[] nums, int k){
         // Here will be finding all possible subarrays till k which are possible
-        int start = 0;
-        int end = 0;
-        int ans = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(end=0; end<nums.length; end++){
-            map.put(nums[end], map.getOrDefault(nums[end],0)+1);
+       int ans = 0;
+       int n = nums.length; 
+       int start = 0;
+       HashMap<Integer, Integer> map = new HashMap<>();
+       for(int right=0; right<n; right++){
+        map.put(nums[right], map.getOrDefault(nums[right],0)+1);
 
-            while(map.size()>k){
-                // Shrinking the window by removing the start pointer 
-                if(map.get(nums[start])>1){
-                    map.put(nums[start], map.get(nums[start])-1);
-                }
-                else{
-                    map.remove(nums[start]);
-                }
-                start++;
-            }
-            ans+=end-start+1;
+        while(map.size()>k){
+
+            if(map.get(nums[start])>1){
+            map.put(nums[start], map.get(nums[start])-1);
         }
-        return ans;
+        else{
+            map.remove(nums[start]);
+        }
+
+        start++;
+       }
+       ans+=right-start+1;
     }
+
+    return ans;
+}
 }
