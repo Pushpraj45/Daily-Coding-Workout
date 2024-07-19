0de4e5fc24e751cbbd321526b3bdf89c69a7c971
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-07-19 19:29:44 +0530

    Time: 972 ms (5.02%) | Memory: 128.7 MB (5.12%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,79 +1,44 @@
-class LRUCache {
-    private final int CAPACITY;
-    private HashMap<Integer, Node> map;
-    private Node head;
-    private Node tail;
-
-    private static class Node {
-        Node prev;
-        Node next;
-        int key;
-        int val;
-
-        Node(int key, int val) {
-            this.key = key;
-            this.val = val;
-        }
-    }
-
-    public LRUCache(int capacity) {
-        CAPACITY = capacity;
-        map = new HashMap<>();
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-        head.next = tail;
-        tail.prev = head;
+class LRUCache
+{
+    int capacity;
+    Queue<Integer> q=new ArrayDeque<>();//faster than LinkedList//to store the key according to  LRU principal
+    Map<Integer,Integer> map=new LinkedHashMap<>();//To store key and value pair 
+    
+    public LRUCache(int capacity) 
+    {
+        this.capacity = capacity;//cache size 
     }
     
-    public int get(int key) {
-        Node res = map.get(key);
-        if (res == null) {
-            return -1;
-        }
-        moveToHead(res);
-        return res.val;
+    public int get(int key)
+    {
+        if (map.containsKey(key)) 
+        {//cache updating 
+            q.remove(key);//O(n)
+            q.offer(key);//O(1)
+            return map.get(key);//O(1)
+        } 
+        else 
+            return -1;//key is npt found 
     }
     
-    public void put(int key, int value) {
-        add(new Node(key, value));
-        if (map.size() > CAPACITY) {
-            removeTail();
+    public void put(int key, int value) 
+    {
+        if(map.containsKey(key)) //if key is present in the map, key has to be searched and deleted and then enqueue the key into the queue, according to LRU principal
+        {
+            q.remove(key);//O(n)//remove the key from the queue 
+            q.offer(key);//O(1)//apeend at the end of the queue(enqueue)
+            map.put(key,value);//O(1)//overwriding the key value (key is always unique)
         }
-    }
-
-    private void add(Node node) {
-        Node old = map.get(node.key);
-        if (old != null) {
-            remove(old);
+        else 
+        {
+            if(q.size() < capacity)
+                q.offer(key);//inserting the new key into the queue 
+            else 
+            {//q.size() == capacity
+                map.remove(q.poll());//removing the least recently used key 
+                q.offer(key);//qppending the new key to the queue
+            }
+            map.put(key,value);//putting the key value pair 
         }
-        map.put(node.key, node);
-        // add to head
-        node.next = head.next;
-        node.prev = head;
-        head.next.prev = node;
-        head.next = node;
-    }
-
-    private void remove(Node node) {
-        map.remove(node.key);
-        // remove from linked list
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-
-    private void moveToHead(Node node) {
-        remove(node);
-        add(node);
-    }
-
-    private void removeTail() {
-        remove(tail.prev);
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
