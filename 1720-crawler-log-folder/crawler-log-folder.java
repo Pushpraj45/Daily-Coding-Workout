0c7c448e010229f3f495c2a5a54e72bc95a13bb3
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-07-27 16:22:56 +0530

    Time: 2 ms (24.30%) | Memory: 41.9 MB (77.14%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> st =new Stack<>();
+
+        for(String s:logs){
+            if(s.equals("../")){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            }
+            else if(!s.equals("./")){
+                st.push(s);
+            }
+        }
+
+        return st.size();
+    }
+}
