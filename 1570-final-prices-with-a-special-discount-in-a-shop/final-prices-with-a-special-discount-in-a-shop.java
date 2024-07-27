ff3c4c79fa37483b2439acb3e03cdd2e8e9b18d7
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-07-27 15:12:33 +0530

    Time: 4 ms (34.25%) | Memory: 44.6 MB (7.13%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,18 +1,36 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-       int n = prices.length;
-        int[] answer = new int[n];
-        System.arraycopy(prices, 0, answer, 0, n);
+    //    int n = prices.length;
+    //     int[] answer = new int[n];
+    //     System.arraycopy(prices, 0, answer, 0, n);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (prices[j] <= prices[i]) {
-                    answer[i] -= prices[j];
-                    break;
-                }
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = i + 1; j < n; j++) {
+    //             if (prices[j] <= prices[i]) {
+    //                 answer[i] -= prices[j];
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return answer;
+
+      int n = prices.length;
+        int[] arr = new int[n];
+        Stack<Integer> st = new Stack<>();
+        
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() > prices[i]) {
+                st.pop();
             }
+            if (st.isEmpty()) {
+                arr[i] = prices[i];
+            } else {
+                arr[i] = prices[i] - st.peek();
+            }
+            st.push(prices[i]);
         }
-        return answer;
+        
+        return arr;
 
     }
 }
