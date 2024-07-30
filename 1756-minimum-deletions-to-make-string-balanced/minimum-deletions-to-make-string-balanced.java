2fb12595ce15eaf418dc6d7bacef3da73f7c0436
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-07-30 23:42:25 +0530

    Time: 132 ms (9.47%) | Memory: 45.3 MB (77.16%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        Stack<Character> st = new Stack<>();
+        int ans = 0;
+        for(char c:s.toCharArray()){
+           
+                if(!st.isEmpty() && st.peek()=='b' && c=='a'){
+                    st.pop();
+                    ans++;
+                }
+                else{
+                    if(c=='b'){
+                        st.push(c);
+                    }
+                }
+            }
+        
+
+        return ans;
+    }
+}
