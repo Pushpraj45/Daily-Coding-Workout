e40b83624e2c670258555b7f5a9fe4599dc8bd9a
Pushpraj Dubey <99044211+Pushpraj45@users.noreply.github.com>
2024-11-21 01:03:11 +0530

    Time: 2 ms (84.01%) | Memory: 45.7 MB (81.43%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,43 +1,13 @@
-class Solution {
-    public void moveZeroes(int[] nums) {
-        // ArrayList<Integer> arr = new ArrayList<>();
-        // for(int i=0; i<nums.length; i++){
-        //     if(nums[i]!=0){
-        //         arr.add(nums[i]);
-        //     }
-        // }
-
-        // int s = arr.size();
-
-        // for(int i=0; i<s; i++){
-        //     nums[i]=arr.get(i);
-        // }
-
-        // for(int i=s; i<nums.length; i++){
-        //     nums[i] = 0;
-        // }
-
-        /* using 2pointer approach here */
-        int j=-1;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i]==0){
-                j=i;
-                break;
-            }
-        }
-
-        if(j==-1){
-            return;
-        }
-
-        for(int i=j+1; i<nums.length; i++){
-            if(nums[i]!=0){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
-            }
-        }
-
-    }
+class Solution{
+ public void moveZeroes(int[]nums){
+  int lastNonZeroIndex=0;
+  for(int i=0;i<nums.length;i++){
+   if(nums[i]!=0){
+    nums[lastNonZeroIndex++]=nums[i];
+   }
+  }
+  for(int i=lastNonZeroIndex;i<nums.length;i++){
+   nums[i]=0;
+  }
+ }
 }
